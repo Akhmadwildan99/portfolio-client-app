4c855d6fd26cc974357d87718f720e832122ff18
Akhmad Wildan <IDSTR.AKHMAD@xl.co.id>
2025-06-08 19:30:59 +0700

    articles data

---
+++ src/data-articles.js
@@ -0,0 +1,30 @@
+ const articles= [
+  {
+    id: 1,
+    title: "State Management di React Vite Berdasarkan Proyek Pokemon-App",
+    date: "May, 1 2025",
+    description:
+      "React telah menjadi salah satu library JavaScript paling populer untuk membangun antarmuka pengguna (UI) yang dinamis dan responsif. Dengan pendekatan berbasis komponen dan kemampuan untuk mengelola state secara efisien, React memungkinkan pengembang menciptakan aplikasi web yang interaktif dan mudah dipelihara.",
+    link: "https://medium.com/@wildante/state-management-di-react-vite-berdasarkan-proyek-pokemon-app-1698c65fec98",
+  },
+  {
+    id: 2,
+    title: "Develop Pokemon Dek Website Menggunakan React Hooks",
+    date: "2025-04-21",
+    description:
+      "Dalam beberapa tahun terakhir, pengembangan aplikasi web telah mengalami kemajuan yang pesat, terutama dengan munculnya pustaka dan...",
+    link: "https://medium.com/@wildante/develop-pokemon-dek-website-menggunakan-react-hooks-ffd562c008c9",
+  },
+  {
+    id: 3,
+    title: "Mengakses Aplikasi Vue dari Ponsel yang diinstall di Server Lokal",
+    date: "2024-09-02",
+    description:
+      "Pertama yang harus diperhatikan adalah ip adress, pastikan ponsel anda dan server lokal anda berada di ip adress yang sama.",
+    link: "https://medium.com/@wildante/mengakses-aplikasi-vue-dari-ponsel-yang-diinstall-di-server-lokal-92f80d5829ce",
+  },
+]
+
+
+
+export default articles;
